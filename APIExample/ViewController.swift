4d29ab7e0c5b9e456fbcb3b36b43804cbb930720
MacOs 10.15 Catalina <macos10.15@MacOss-Mac.local>
2021-01-04 04:01:00 -0800

    Inital commit

--- APIExample/ViewController.swift
+++ APIExample/ViewController.swift
@@ -9,19 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = "Fetching courses please wait..."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         getData()
     }
     
     func getData(){
         let anonymousfunction = {(fetchedResultList:CourseModel) in
             let data  = fetchedResultList.result?.results
-            print("-=-=-=-=-=-=-=-= COURSES -=-=-=-=-=-=-=-=")
-            for i in 0..<data!.count{
-                print(data![i].name!)
+            
+            DispatchQueue.main.async {
+                print("-=-=-=-=-=-=-=-= COURSES -=-=-=-=-=-=-=-=")
+                for i in 0..<data!.count{
+                    print(data![i].name!)
+                    self.label.text = "Courses Fetched check terminal"
+                }
             }
+
         }
         
         CourseController().getCourses(onCompletion: anonymousfunction)
